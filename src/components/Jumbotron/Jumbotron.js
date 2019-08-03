da5c58619ba30f03e9f697b950fb345a2a038359
Mad Marine <pnnroman92@gmail.com>
2019-08-03 13:42:06 -0500

    edited texts

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -3,7 +3,8 @@ import React from "react";
 const Jumbotron = ({ children }) => (
   <div className="jumbotron jumbotron-fluid">
     <div 
-      style={{ height: 150, clear: "both", paddingTop: 10, paddingBottom: 20, textAlign: "Center"}}
+      style={{ height: 150, clear: "both", paddingTop: 10, paddingBottom: 20, textAlign: "Center",
+      backgroundImage: `url("https://images.unsplash.com/photo-1535905557558-afc4877a26fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60")`}}
       className="container">
       {children}
     </div>
--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -49,8 +49,8 @@ class Search extends Component {
     return (
       <div>
         <Jumbotron>
-          <h1 className="display-4">(React) Google Books Search</h1>
-          <p className="lead">Search for and save books of interest.</p>
+          <h1 className="display-4">Let's Find You Something to Read!</h1>
+          <p className="lead">Find a Book and Save it. It's That Simple!</p>
           <hr className="my-4" />
           <p className="lead">
             <Link className="btn btn-default btn-lg" to="/" role="button">New Search</Link>
