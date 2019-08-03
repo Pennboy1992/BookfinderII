bd05f8dc103e54c370eed9641ea1ff17fa87a0bc
Mad Marine <pnnroman92@gmail.com>
2019-08-03 13:47:32 -0500

    changed text color to match background

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -4,7 +4,7 @@ const Jumbotron = ({ children }) => (
   <div className="jumbotron jumbotron-fluid">
     <div 
       style={{ height: 150, clear: "both", paddingTop: 10, paddingBottom: 20, textAlign: "Center",
-      backgroundImage: `url("https://images.unsplash.com/photo-1535905557558-afc4877a26fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60")`}}
+      backgroundImage: `url("https://images.unsplash.com/photo-1535905557558-afc4877a26fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60")`, color: "white"}}
       className="container">
       {children}
     </div>
